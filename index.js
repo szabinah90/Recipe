1a9896952fdeda5300428fb040baff2fb9e69081
Szabina Hazi <szabina.hazi@flowacademy.hu>
2018-03-01 15:34:03 +0100

    view first commit

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const index = express();
-
+const expressHandlebars = require('express-handlebars');
 const recipes = require('./controllers/recipes');
+
+// handlebars
+const handlebarsOptions = { defaultLayout: 'main' };
+const handlebarsConfig = expressHandlebars(handlebarsOptions);
+index.engine('handlebars', handlebarsConfig);
+index.set('view engine', 'handlebars');
+// index.engine('handlebars', expressHandlebars({defaultLayout: 'main'})); ugyanaz, mint a fentebbi 4 sor, csak szétszedtük az érthetőség kedvéért. 
+
+index.get('/pelda', (req, res) => {
+  res.render('geza');
+});
+
+index.get('/juli', (req, res) => {
+  res.render('valami');
+});
 
 // logger
 const logger = (req, res, next) => {
-  console.log(req.method);
+  console.log(req.method, req.path);
   next();
 };
 
-// index.use(logger);
+index.use(logger);
 index.use('/recipes', recipes);
 
 // LISTENER
